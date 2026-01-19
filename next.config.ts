58487aac74b0af8c7203888583f207f044be4313
viktor <victorand804@gmail.com>
2026-01-19 12:12:40 +0100

    Fix Vercel build: Remove next-video dependency, update build config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
-import { withNextVideo } from "next-video/process";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withNextVideo(nextConfig);
+export default nextConfig;
